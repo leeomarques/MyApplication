90d0b6cc069f8d74e481bbb1d817855ec4ab1be2
Leonardo Marques <leonardogm_16@hotmail.com>
2017-09-29 05:09:34 -0300

    Criação do Edit Text e Button  Mostrando um Toast

--- app/src/main/java/com/example/leonardo/aprendendo/MainActivity.java
+++ app/src/main/java/com/example/leonardo/aprendendo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.leonardo.aprendendo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText edt = (EditText) findViewById(R.id.edtTexto);
+        Button btn = (Button) findViewById(R.id.button);
+
+        //Metodo de Click
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String texto = edt.getText().toString();
+                Toast.makeText(MainActivity.this, texto, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
